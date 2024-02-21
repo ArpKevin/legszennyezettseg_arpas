7d8f3e2b65523ef781e135cf975aa2f5fdeddd6f
Kevin <arpaskevin11@gmail.com>
2024-02-21 11:21:00 +0100

    Add project files.

---
+++ legszennyezettseg_arpas/Legszennyezes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legszennyezettseg_arpas
+{
+    internal class Legszennyezes
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ legszennyezettseg_arpas/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace legszennyezettseg_arpas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var legszennyezesek = new Dictionary<int, List<int>>();
+
+            //foreach (var sor in File.ReadAllLines(@"..\..\..\src\SO2.txt"))
+            //{
+            //    legszennyezesek.Add(sor.Select((e, i) => i, sor.Split(";").ToList()));
+            //}
+
+            var lines = File.ReadAllLines(@"..\..\..\src\SO2.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(';').Select(int.Parse).ToList();
+                legszennyezesek.Add(i + 1, values);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
